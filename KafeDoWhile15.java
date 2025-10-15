b73053b837dec350cd0646c8776ff73bf3b03eca
Hazbilluh-source <254107020167@student.polinema.ac.id>
2025-10-15 14:29:45 +0700

    dojava1

---
+++ KafeDoWhile15.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class KafeDoWhile15 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int kopi;
+        int teh;
+        int roti;
+        int totalHarga;
+        String namaPelanggan;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+        do {
+            System.out.println("Masukkan nama pelanggan (ketik `batal` untuk keluar)");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equals("batal")) {
+            System.out.println("Transaksi dibatalkan.");
+            break;
+
+            }
+            System.out.print("jumlah kopi");
+            kopi = sc.nextInt();
+            System.out.print("jumlah teh");
+            teh = sc.nextInt();
+            System.err.print("jumlah roti");
+            roti = sc.nextInt();
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            
+        } while (true);
+        System.out.println("Semua transaksi selesai");
+    }
+}
